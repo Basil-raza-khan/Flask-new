d5eb36a1e66da7de402e974c2e54ff0b29a9cfce
Basil-raza-khan <15062@kiet.edu.pk>
2024-06-21 12:13:18 +0500

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         
-    app.run(debug=True)
+    app.run(debug=False ,host="0.0.0.0")
     
 # For database creation
 # >>> from app import app, db
